b34bebdc2cf1beb99dd9381cceb8a32de65648b7
Hitanshi <hitanshishah2002@gmail.com>
2023-02-17 09:23:29 +0530

    add another file

--- E-1/user session/user session.js
+++ E-1/user session/user session.js
@@ -1,45 +1,4 @@
-// const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
-// if(currentUser){
-//   const adminName = document.getElementById("adminName");
-//   adminName.textContent = currentUser.name;
-// }
-
-// const admin = JSON.parse(localStorage.getItem("users"));
-// console.log(admin);
-
-// const table = document.getElementById("table");
-// admin.forEach((item) => {
-//   const row = document.createElement("tr");
-
-//   const nameCell = document.createElement("td");
-//   const startTime = document.createElement("td");
-//   const endTime = document.createElement("td");
-
-//   nameCell.textContent = item.name;
-//   row.appendChild(nameCell);
-
-//   if(item.loginTime){
-//     startTime.textContent = item.loginTime;
-//     row.appendChild(startTime);
-//   }else{
-//     startTime.textContent = "Not Logged In Yet";
-//     row.appendChild(startTime); 
-//   }
-
-//   if(item.logoutTime){
-//     endTime.textContent = item.logoutTime;
-//     row.appendChild(endTime);
-//   }else{
-//     endTime.textContent = "Not Logged Out Yet";
-//     row.appendChild(endTime);
-//   }
-//   table.appendChild(row);
-// })
-
-// function logout(){
-    
-// }
 
 const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
